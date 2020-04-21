7cbc6f33c66de47294626f3b3fd5d73be79db889
y.mikhin <y.mikhin@fun-box.ru>
2020-04-21 23:31:23 +0300

    Исправление тестов.

--- test/index.test.js
+++ test/index.test.js
@@ -4,20 +4,22 @@ import { AsyncArray } from '../src/async-array';
 test('Сложение векторов происходит правильно.', async () => {
   const v1 = new AsyncArray([1, 2, 3]);
   const v2 = new AsyncArray([3, 4, 5]);
+  const callback = jest.fn((x) => x);
+  await sumVectors(v1, v2, callback);
 
-  expect(sumVectors(v1, v2)).resolves.toBe('4,6,8');
+  return expect(callback.mock.calls[0][0].getArrayForTests()).toMatchObject([4, 6, 8]);
 });
 
 test('Векторы не соразмерны; ошибка.', async () => {
   const v1 = new AsyncArray([1, 2, 3]);
   const v2 = new AsyncArray([3, 4, 5, 6]);
 
-  expect(sumVectors(v1, v2)).rejects.toEqual(new Error('Векторы несоразмерны'));
+  return expect(sumVectors(v1, v2)).rejects.toEqual(new Error('Векторы несоразмерны'));
 });
 
 test('Векторы не соприкасаются; ошибка.', async () => {
   const v1 = new AsyncArray([1, 2, 3]);
   const v2 = new AsyncArray([4, 5, 6]);
 
-  expect(sumVectors(v1, v2)).rejects.toEqual(new Error('Векторы не соприкасаются'));
+  return expect(sumVectors(v1, v2)).rejects.toEqual(new Error('Векторы не соприкасаются'));
 });
